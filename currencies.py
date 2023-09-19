d75eff7a17349491f3b31d27e62f7e9795317220
Hugo <28987448+hugoadventist@users.noreply.github.com>
2023-09-19 18:52:48 -0300

    Revert "Revert "revert-preparation""
    This reverts commit c74b1ee3e1a533eaba714ff3d63f1d321a7e71a6.

--- currencies.py
+++ currencies.py
@@ -2,6 +2,3 @@ def parser_currencies(currencies="BRL-USD,BRL-EUR,BRL-INR"):
     if not isinstance(currencies, str):
         raise TypeError("Favor inserir uma lista com os parâmetros válidos!")
     return currencies
-
-
-parser_currencies()
