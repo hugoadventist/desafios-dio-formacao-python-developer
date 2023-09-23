9d347b13390011cc4ea4ebfadb200fd0053c912d
Hugo Riviere <28987448+hugoadventist@users.noreply.github.com>
2023-09-23 04:41:57 -0300

    Delete desafios directory

--- desafios/currencies.py
+++
@@ -1,7 +0,0 @@
-def parser_currencies(currencies="BRL-USD,BRL-EUR,BRL-INR"):
-    if not isinstance(currencies, str):
-        raise TypeError("Favor inserir uma lista com os parâmetros válidos!")
-    return currencies
-
-
-parser_currencies()
