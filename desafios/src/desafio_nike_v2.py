49836db0c31cd1c131035154e96e9091e741eaea
Hugo <28987448+hugoadventist@users.noreply.github.com>
2023-09-23 04:31:23 -0300

    feature pronta para fazer o merge

---
+++ desafios/src/desafio_nike_v2.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI
+import aiohttp
+
+app = FastAPI()
+
+
+def transformar_entrada(entradas: str):
+    saidas = entradas.replace("-", "").split(",")
+    return saidas
+
+
+def extrair_dados_json(json_file: dict, coins: list, key: str):
+    exchange_rates = {}
+    for currency in coins:
+        exchange_rates[currency] = json_file[currency][key]
+    return exchange_rates
+
+
+def converter_valores(value: float, last_exchange: dict):
+    for currency in last_exchange:
+        last_exchange[currency] = float(last_exchange[currency])
+        last_exchange[currency] *= value
+    return {key: round(last_exchange[key], 2) for key in last_exchange}
+
+
+@app.get("/api/convert/BRL/{valor}")
+async def receber_valor(valor: float, moedas: str = "BRL-USD,BRL-EUR,BRL-INR"):
+    if moedas:
+        moedas += ",BRL-USD,BRL-EUR,BRL-INR"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            "https://economia.awesomeapi.com.br/last/" + moedas
+        ) as resp:
+            print(resp.status)
+            exchange_json = await resp.json()
+
+    # exchange_json = json.dumps(resp, sort_keys=False, indent=4)
+    currency_list = transformar_entrada(moedas)
+    print(currency_list)
+
+    valores_moedas = extrair_dados_json(
+        json_file=exchange_json, coins=currency_list, key="bid"
+    ).copy()
+
+    print("Valor total: ", valores_moedas)
+
+    valores_convertidos = converter_valores(valor, valores_moedas)
+
+    return valores_convertidos
+
+
+# exchanges = aiohttp.request(
+#     "GET", "https://economia.awesomeapi.com.br/last/BRL-USD,BRL-EUR,BRL-INR"
+# )
